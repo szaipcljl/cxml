45489c1e8d200679b97796a81e4c8864297960e6
Ludvig Rydahl <c14lrl@cs.umu.se>
2018-10-02 10:13:09 +0200

    Implemented reallocWithHdr, freeAlloced, copy and ncopy

--- src/utils/CXmlString.c
+++ src/utils/CXmlString.c
@@ -1,97 +1,98 @@
 #include "CXmlString.h"
+#include "CXmlPreProcessor.h"
+#include "CXmlErrno.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <zconf.h>
 #include <ctype.h>
 
-#define CXMLSTRING_HDR_MARKER char
-#define CXMLSTRING_HDR_MARKER_SIZE sizeof(CXMLSTRING_HDR_MARKER)
+#define CXMLS_HDR_MARKER char
+#define CXMLS_HDR_MARKER_SIZE sizeof(CXMLS_HDR_MARKER)
 
-#define CXMLSTRING_ACCESS_HDR(size, str) \
-    ((struct CXmlStringHdr##size*)((str)-sizeof(struct CXmlStringHdr##size)))
+#define CXMLS_ACCESS_HDR(size, str) \
+    ((struct CXmlStringHdr##size*)((str)-(sizeof(struct CXmlStringHdr##size)+CXMLS_HDR_MARKER_SIZE)))
 
-#define CXMLSTRING_GET_HDRTYPE(str) \
-    ((enum CXmlStringHdrType)(*(CXMLSTRING_HDR_MARKER*)((str)-1)))
+#define CXMLS_GET_HDRTYPE(str) \
+    ((enum CXmlStringHdrType)(*(CXMLS_HDR_MARKER*)((str)-1)))
 
-#define CXMLSTRING_GET_HDRSIZE(str) \
-    (CXMLSTRING_GET_HDRTYPE(str)*8)
+#define CXMLS_GET_HDRSIZE(str) \
+    (CXMLS_GET_HDRTYPE(str)*8)
 
-#define CXMLSTRING_APPLY_TERMINATOR(str, len)\
+#define CXMLS_APPLY_TERMINATOR(str, len)\
     do{(str)[len] = '\0';}while(0)
 
-#define CXmlString_getLen(str, out_len)\
+#define CXMLS_GET_LEN(str, out_len)\
     do{\
-        switch(CXMLSTRING_GET_HDRTYPE(str)){\
+        switch(CXMLS_GET_HDRTYPE(str)){\
             case CXMLSTRING_HDR8:  \
-                (out_len) = CXMLSTRING_ACCESS_HDR(8, str)->len;\
+                (out_len) = CXMLS_ACCESS_HDR(8, str)->len;\
                 break;\
             case CXMLSTRING_HDR16:  \
-                (out_len) = CXMLSTRING_ACCESS_HDR(16, str)->len;\
+                (out_len) = CXMLS_ACCESS_HDR(16, str)->len;\
                 break;\
             case CXMLSTRING_HDR32:  \
-                (out_len) = CXMLSTRING_ACCESS_HDR(32, str)->len;\
+                (out_len) = CXMLS_ACCESS_HDR(32, str)->len;\
                 break;\
             case CXMLSTRING_HDR64:  \
-                (out_len) = CXMLSTRING_ACCESS_HDR(64, str)->len;\
+                (out_len) = CXMLS_ACCESS_HDR(64, str)->len;\
                 break;\
             default:\
                 (out_len) = 0;\
         }\
     }while(0)
 
-#define CXmlString_getSize(str, out_size)\
+#define CXMLS_GET_SIZE(str, out_size)\
     do{\
-        switch(CXMLSTRING_GET_HDRTYPE(str)){\
+        switch(CXMLS_GET_HDRTYPE(str)){\
             case CXMLSTRING_HDR8:  \
-                (out_size) = CXMLSTRING_ACCESS_HDR(8, str)->len;\
+                (out_size) = CXMLS_ACCESS_HDR(8, str)->len;\
                 break;\
             case CXMLSTRING_HDR16:  \
-                (out_size) = CXMLSTRING_ACCESS_HDR(16, str)->len;\
+                (out_size) = CXMLS_ACCESS_HDR(16, str)->len;\
                 break;\
             case CXMLSTRING_HDR32:  \
-                (out_size) = CXMLSTRING_ACCESS_HDR(32, str)->len;\
+                (out_size) = CXMLS_ACCESS_HDR(32, str)->len;\
                 break;\
             case CXMLSTRING_HDR64:  \
-                (out_size) = CXMLSTRING_ACCESS_HDR(64, str)->len;\
+                (out_size) = CXMLS_ACCESS_HDR(64, str)->len;\
                 break;\
             default:\
                 (out_size) = 0;\
         }\
     }while(0)
 
-#define CXmlString_setLen(str, newlen)                                          \
+#define CXMLS_SET_LEN(str, newlen)                                          \
     do{                                                                         \
-        switch(CXMLSTRING_GET_HDRTYPE(str)){                                    \
+        switch(CXMLS_GET_HDRTYPE(str)){                                    \
             case CXMLSTRING_HDR8:                                               \
-                CXMLSTRING_ACCESS_HDR(8, str)->len = (uint8_t)(newlen);         \
+                CXMLS_ACCESS_HDR(8, str)->len = (uint8_t)(newlen);         \
                 break;                                                          \
             case CXMLSTRING_HDR16:                                              \
-                CXMLSTRING_ACCESS_HDR(16, str)->len = (uint16_t)(newlen);       \
+                CXMLS_ACCESS_HDR(16, str)->len = (uint16_t)(newlen);       \
                 break;                                                          \
             case CXMLSTRING_HDR32:                                              \
-                CXMLSTRING_ACCESS_HDR(32, str)->len = (uint32_t)(newlen);       \
+                CXMLS_ACCESS_HDR(32, str)->len = (uint32_t)(newlen);       \
                 break;                                                          \
             case CXMLSTRING_HDR64:                                              \
-                CXMLSTRING_ACCESS_HDR(64, str)->len = newlen;                   \
+                CXMLS_ACCESS_HDR(64, str)->len = newlen;                   \
                 break;                                                          \
         }                                                                       \
     }while(0)
 
-#define CXmlString_setSize(str, newsize)                                        \
+#define CXMLS_SET_SIZE(str, newsize)                                        \
     do{                                                                         \
-        switch(CXMLSTRING_GET_HDRTYPE(str)){                                    \
+        switch(CXMLS_GET_HDRTYPE(str)){                                    \
             case CXMLSTRING_HDR8:                                               \
-                CXMLSTRING_ACCESS_HDR(8, str)->size = (uint8_t)(newsize);       \
+                CXMLS_ACCESS_HDR(8, str)->size = (uint8_t)(newsize);       \
                 break;                                                          \
             case CXMLSTRING_HDR16:                                              \
-                CXMLSTRING_ACCESS_HDR(16, str)->size = (uint16_t)(newsize);     \
+                CXMLS_ACCESS_HDR(16, str)->size = (uint16_t)(newsize);     \
                 break;                                                          \
             case CXMLSTRING_HDR32:                                              \
-                CXMLSTRING_ACCESS_HDR(32, str)->size = (uint32_t)(newsize);     \
+                CXMLS_ACCESS_HDR(32, str)->size = (uint32_t)(newsize);     \
                 break;                                                          \
             case CXMLSTRING_HDR64:                                              \
-                CXMLSTRING_ACCESS_HDR(64, str)->size = newsize;                 \
+                CXMLS_ACCESS_HDR(64, str)->size = newsize;                 \
                 break;                                                          \
         }                                                                       \
     }while(0)
@@ -123,7 +124,7 @@ struct CXmlStringHdr64{
     uint64_t size;
 };
 
-static inline size_t CXmlString_getHdrSize(enum CXmlStringHdrType hdrType)
+static inline size_t getHdrSize(enum CXmlStringHdrType hdrType)
 {
     switch(hdrType){
         case CXMLSTRING_HDR8:  return sizeof(struct CXmlStringHdr8);
@@ -134,15 +135,7 @@ static inline size_t CXmlString_getHdrSize(enum CXmlStringHdrType hdrType)
     }
 }
 
-static inline CXmlString *CXmlString_alloc(size_t size, enum CXmlStringHdrType hdrType)
-{
-    size_t hdrSize = CXmlString_getHdrSize(hdrType);
-    CXmlString *ptr = malloc(size+hdrSize+CXMLSTRING_HDR_MARKER_SIZE);
-    *(CXMLSTRING_HDR_MARKER*)(ptr+hdrSize) = hdrType;
-    return ptr+hdrSize+CXMLSTRING_HDR_MARKER_SIZE;
-}
-
-static inline enum CXmlStringHdrType CXmlString_getHdrType(size_t size)
+static inline enum CXmlStringHdrType getHdrType(size_t size)
 {
     if(size < 1 << 8U) return CXMLSTRING_HDR8;
     if(size < 1 << 16) return CXMLSTRING_HDR16;
@@ -154,7 +147,69 @@ static inline enum CXmlStringHdrType CXmlString_getHdrType(size_t size)
 #endif
 }
 
-//static CXmlString
+
+static inline CXmlString *allocWithHdr(size_t size)
+{
+    enum CXmlStringHdrType hdrType = getHdrType(size);
+    size_t hdrSize = getHdrSize(hdrType);
+    CXmlString *ptr = malloc(size+hdrSize+CXMLS_HDR_MARKER_SIZE);
+    if(ptr == NULL){
+        cxmlerrno = CXML_EALLOC;
+        return NULL;
+    }
+    *(CXMLS_HDR_MARKER*)(ptr+hdrSize) = hdrType;
+    return ptr+hdrSize+CXMLS_HDR_MARKER_SIZE;
+}
+
+static inline void freeAlloced(CXmlString *str)
+{
+    switch(CXMLS_GET_HDRTYPE(str)) {
+        case CXMLSTRING_HDR8:
+            free(CXMLS_ACCESS_HDR(8, str));
+            break;
+        case CXMLSTRING_HDR16:
+            free(CXMLS_ACCESS_HDR(16, str));
+            break;
+        case CXMLSTRING_HDR32:
+            free(CXMLS_ACCESS_HDR(32, str));
+            break;
+        case CXMLSTRING_HDR64:
+            free(CXMLS_ACCESS_HDR(64, str));
+            break;
+    }
+}
+
+static inline CXmlString *reallocWithHdr(CXmlString *str, size_t size)
+{
+    size_t origLen = 0;
+    if(CXML_NOTNULL(str)){
+        CXMLS_GET_LEN(str, origLen);
+    }
+    enum CXmlStringHdrType hdrType = getHdrType(size);
+    size_t hdrSize = getHdrSize(hdrType);
+
+    switch(CXMLS_GET_HDRTYPE(str)) {
+        case CXMLSTRING_HDR8:
+            str = realloc(CXMLS_ACCESS_HDR(8, str), size+hdrSize+CXMLS_HDR_MARKER_SIZE);
+            break;
+        case CXMLSTRING_HDR16:
+            str = realloc(CXMLS_ACCESS_HDR(16, str), size+hdrSize+CXMLS_HDR_MARKER_SIZE);
+            break;
+        case CXMLSTRING_HDR32:
+            str = realloc(CXMLS_ACCESS_HDR(32, str), size+hdrSize+CXMLS_HDR_MARKER_SIZE);
+            break;
+        case CXMLSTRING_HDR64:
+            str = realloc(CXMLS_ACCESS_HDR(64, str), size+hdrSize+CXMLS_HDR_MARKER_SIZE);
+            break;
+    }
+    if(str == NULL){
+        cxmlerrno = CXML_EALLOC;
+        return NULL;
+    }
+    *(CXMLS_HDR_MARKER*)(str+hdrSize) = hdrType;
+    CXMLS_SET_LEN(str, origLen);
+    return str+hdrSize+CXMLS_HDR_MARKER_SIZE;
+}
 
 CXmlString *CXmlString_new(const char *str)
 {
@@ -163,13 +218,14 @@ CXmlString *CXmlString_new(const char *str)
 
 CXmlString *CXmlString_newLen(const char *str, size_t len)
 {
-    enum CXmlStringHdrType type = CXmlString_getHdrType(len);
-    CXmlString *newstr = CXmlString_alloc(len, type);
+    CXML_RETNULL_IFNULL(str);
+    CXmlString *newstr = allocWithHdr(len);
+    if(newstr == NULL) return NULL;
     memset(newstr, '\0', len);
     memcpy(newstr, str, len);
-    CXMLSTRING_APPLY_TERMINATOR(newstr, len);
-    CXmlString_setLen(newstr, strlen(newstr));
-    CXmlString_setSize(newstr, len);
+    CXMLS_APPLY_TERMINATOR(newstr, len);
+    CXMLS_SET_LEN(newstr, strlen(newstr));
+    CXMLS_SET_SIZE(newstr, len);
     return newstr;
 }
 
@@ -181,21 +237,21 @@ CXmlString *CXmlString_empty()
 size_t CXmlString_len(const CXmlString *str)
 {
     size_t len;
-    CXmlString_getLen(str, len);
+    CXMLS_GET_LEN(str, len);
     return len;
 }
 
 int CXmlString_isEmpty(const CXmlString *str)
 {
     size_t len;
-    CXmlString_getLen(str, len);
+    CXMLS_GET_LEN(str, len);
     return len == 0;
 }
 
 CXmlString *CXmlString_dup(const CXmlString *str)
 {
     size_t len;
-    CXmlString_getLen(str, len);
+    CXMLS_GET_LEN(str, len);
     return CXmlString_newLen(str, len);
 }
 
@@ -204,25 +260,35 @@ CXmlString *CXmlString_trim(const CXmlString *str)
     NOT_IMPLEMENTED //TODO Implement
 }
 
-CXmlString *CXmlString_copy(CXmlString *dest, const CXmlString *src)
+CXmlString *CXmlString_copy(CXmlString **dest, const CXmlString *src)
 {
+    CXML_RETNULL_IFNULL(src);
+    size_t srclen;
+    CXMLS_GET_LEN(src, srclen);
+    return CXmlString_ncopy(dest, src, srclen);
+}
+
+CXmlString *CXmlString_ncopy(CXmlString **dest, const CXmlString *src, size_t len)
+{
+    CXML_RETNULL_IFNULL(dest);
+    CXML_RETNULL_IFNULL(src);
     size_t destSize;
-    size_t srcLen;
-    CXmlString_getSize(dest, destSize);
-    CXmlString_getLen(src, srcLen);
-    if(srcLen > destSize){
-        //Need to realloc
-        enum CXmlStringHdrType newHdrType = CXmlString_getHdrType(srcLen);
-        CXmlString_realloc(dest, srcLen, newHdrType);
+    CXMLS_GET_SIZE(*dest, destSize);
+
+    if(len > destSize){
+        //Need to realloc *dest to fit
+
+        enum CXmlStringHdrType newHdrType = getHdrType(len);
+        CXmlString *tmp = reallocWithHdr(*dest, len);
+        if(tmp == NULL) return NULL;
+        *dest = tmp;
     }
 
-    memcpy(dest, src, srcLen);
-    CXmlString_setSize(dest, srcLen);
-    CXmlString_setLen(dest, srcLen);
-    return dest;
+    memcpy(*dest, src, len);
+    CXMLS_SET_SIZE(*dest, destSize > len ? destSize : len);
+    CXMLS_SET_LEN(*dest, len);
+    return *dest;
 }
 
-CXmlString *CXmlString_ncopy(CXmlString *dest, const CXmlString *src, size_t len)
-{
-    NOT_IMPLEMENTED //TODO Implement
-}
+
+
